2212e0f69266dd4c85cb40a5418929988491d0b6
Suraj Kumar <56875771+surajkr122@users.noreply.github.com>
2022-04-10 08:27:39 +0530

    Delete question4.java

--- question4.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-public class question4 {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        double d=sc.nextDouble();
-        System.out.println("cube ="+d*d*d);
-    }
-}
